8b7be5a3305b00c94aa339bdef9a15d3e279a777
NicolasZulaica <nzulaica@fi.uba.ar>
2020-10-12 18:52:16 -0300

    test

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "evento_pesca.h"
 
 int main(int argc, char const *argv[]) {
 
-  printf("Hello World\n");
+  FILE* arrecife_f = fopen( "arrecife.txt", "w" );
+  fclose(arrecife_f);
 
   return 0;
 }
